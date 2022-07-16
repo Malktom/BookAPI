375f4593d57edbf26f307a27b57c1260257ad837
Malktom <tomasz.malkiewicz@yahoo.com>
2022-07-16 15:17:42 +0200

    books list

--- src/main/java/pl/coderslab/controller/BookController.java
+++ src/main/java/pl/coderslab/controller/BookController.java
@@ -1,16 +1,34 @@
 package pl.coderslab.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.coderslab.model.Book;
+import pl.coderslab.repository.MockBookService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/books")
 public class BookController {
 
+    private MockBookService mockBookService;
+
+    @Autowired
+    public BookController(MockBookService mockBookService) {
+        this.mockBookService = mockBookService;
+    }
+
     @RequestMapping("/helloBook")
     public Book helloBook() {
         return new Book(1L, "9788324631766", "Thinking in Java",
                 "Bruce Eckel", "Helion", "programming");
+    }
+
+    @GetMapping("books")
+    public List<Book> getBooksList () {
+
+        return mockBookService.getList();
     }
 }
--- src/main/java/pl/coderslab/repository/MockBookService.java
+++ src/main/java/pl/coderslab/repository/MockBookService.java
@@ -1,11 +1,21 @@
-package pl.coderslab.model;
+package pl.coderslab.repository;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import pl.coderslab.model.Book;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
+@Data
 public class MockBookService {
 
     private List<Book> list;
+    private static Long nextId = 4L;
+
+
 
     public MockBookService() {
         list = new ArrayList<>();
@@ -15,4 +25,6 @@ public class MockBookService {
         list.add(new Book(3L, "9780130819338", "Java	2.	Podstawy", "Cay	Horstmann,	Gary	Cornell", "Helion",
                 "programming"));
     }
+
+
 }
