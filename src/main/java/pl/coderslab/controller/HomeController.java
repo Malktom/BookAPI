4f7ce7eefb4649f4cd04eabeedfa758441cfc157
Malktom <tomasz.malkiewicz@yahoo.com>
2022-08-13 17:33:57 +0200

    Book API Hibernate

---
+++ src/main/java/pl/coderslab/controller/HomeController.java
@@ -0,0 +1,19 @@
+package pl.coderslab.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+
+public class HomeController {
+
+
+
+    @GetMapping("/")
+    public String homePage() {
+
+        return "redirect:/admin/books/all";
+    }
+}
--- src/main/java/pl/coderslab/model/Book.java
+++ src/main/java/pl/coderslab/model/Book.java
@@ -3,18 +3,36 @@ package pl.coderslab.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name="books")
 public class Book {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+   @NotBlank
     private String isbn;
+    @Length(min=3)
     private String title;
     private String author;
     private String publisher;
     private String type;
 
 
+    @Override
+    public String toString() {
+        return "id:" +this.id +" "+"\n"+
+                "isbn: " +this.isbn +" "+"\n"+
+                "title: " +this.title + " " +"\n"+
+                "author: " + this.author + " " +"\n"+
+                "publisher: " +this.publisher + " "+"\n"+
+                "type: " +this.type;
+    }
 }
